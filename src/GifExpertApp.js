7e1bc4d0945377be8e87c656596957c749057ff3
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-06-23 17:28:17 -0700

    Removed unused htmltags.

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -7,21 +7,19 @@ export const GifExpertApp = () => {
 
 	return (
 		<>
-			<section>
-				<h2>Gif Expert App</h2>
-				<AddCategory setCategories={setCategories} />
-				<hr />
-				<ol>
-					{
-						categories.map((category) => (
-							<GifGrid
-								key={category}
-								category={category}
-							/>
-						))
-					}
-				</ol>
-			</section>
+			<h2>Gif Expert App</h2>
+			<AddCategory setCategories={setCategories} />
+			<hr />
+			<ol>
+				{
+					categories.map((category) => (
+						<GifGrid
+							key={category}
+							category={category}
+						/>
+					))
+				}
+			</ol>
 		</>
 	)
 }
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -11,14 +11,12 @@ export const GifGrid = ({ category }) => {
 			<h3 className="animate__animated animate__fadeIn">{category}</h3>
 			{loading && <p className="animate__animated animate__flash">Cargando</p>}
 			<div className="card-grid">
-				<ol>
-					{images.map((img) => (
-						<GifGridItem
-							key={img.id}
-							{...img}
-						/>
-					))}
-				</ol>
+				{images.map((img) => (
+					<GifGridItem
+						key={img.id}
+						{...img}
+					/>
+				))}
 			</div>
 		</>
 	)
